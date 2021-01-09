f179c567d05a1480a4a24db52df11aa5efb4c014
nag763 <loic.labeye@tutanota.de>
2021-01-10 00:43:19 +0100

    End of logic for online part

--- js/onl_team.js
+++ js/onl_team.js
@@ -3,14 +3,15 @@ const $ = require('jquery');
 
 import {generateGetRequest} from './utils/httputils.js';
 import {generateHTMLtr} from './utils/htmlutils.js';
+import {Fixture} from './classes/fixture.js';
 
 const query = querystring.parse(global.location.search);
 const idToDisplay = JSON.parse(query['?id']);
 
 
-const url = `teams/team/${idToDisplay}`;
+const urlTeam = `teams/team/${idToDisplay}`;
 
-generateGetRequest(url).then((response) => {
+generateGetRequest(urlTeam).then((response) => {
   const team = response.data.api.teams[0];
   const infosTable = $('#infos');
 
@@ -22,11 +23,42 @@ generateGetRequest(url).then((response) => {
   infosTable.append(generateHTMLtr(['Ville', team.venue_city]));
   infosTable.append(generateHTMLtr(['Capacité stade', team.venue_capacity]));
 });
-const urlFixtures = `fixtures/team/${idToDisplay}/10`;
 
-generateGetRequest(urlFixtures).then(response => {
-    console.log(response.data)
-})
+const urlLastFixtures = `fixtures/team/${idToDisplay}/last/5`;
+
+generateGetRequest(urlLastFixtures).then((response) => {
+  console.log(response.data);
+  const table = $('#latestFixtures');
+  response.data.api.fixtures.forEach((element) => {
+    const fixture = new Fixture();
+    fixture.league = element.league.name;
+    fixture.homeTeamName = element.homeTeam.team_name;
+    fixture.awayTeamName = element.awayTeam.team_name;
+    fixture.goalsHomeTeam = element.goalsHomeTeam;
+    fixture.goalsAwayTeam = element.goalsAwayTeam;
+    fixture.eventDate = element.event_date;
+    fixture.leagueName = element.league.name;
+    fixture.leagueId = element.league_id;
+    table.append(generateHTMLtr([fixture.eventHourTime(), fixture.homeTeamName, fixture.awayTeamName, fixture.fullscore(), `<a href='./onl_league.html?id=${fixture.leagueId}'>${fixture.leagueName}</a>`]));
+  });
+});
+
+const urlNextFixtures = `fixtures/team/${idToDisplay}/next/5`;
+
+generateGetRequest(urlLastFixtures).then((response) => {
+  console.log(response.data);
+  const table = $('#nextFixtures');
+  response.data.api.fixtures.forEach((element) => {
+    const fixture = new Fixture();
+    fixture.league = element.league.name;
+    fixture.homeTeamName = element.homeTeam.team_name;
+    fixture.awayTeamName = element.awayTeam.team_name;
+    fixture.eventDate = element.event_date;
+    fixture.leagueName = element.league.name;
+    fixture.leagueId = element.league_id;
+    table.append(generateHTMLtr([fixture.eventHourTime(), fixture.homeTeamName, fixture.awayTeamName, `<a href='./onl_league.html?id=${fixture.leagueId}'>${fixture.leagueName}</a>`]));
+  });
+});
 
 $('#go_back').click( () => {
   $(location).attr('href', './menu.html');
