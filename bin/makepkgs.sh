c9bf0c7d9d122e1fb007b15abe528bbb198aba5b
nag763 <loic.labeye@tutanota.de>
2021-01-24 15:52:02 +0100

    Add pkg maker

---
+++ bin/makepkgs.sh
@@ -0,0 +1,9 @@
+#!usr/bin/sh
+
+electron-packager ./ --icon=./icons/icon.png --platform=linux --overwrite
+electron-packager ./ --icon=./icons/icon.png --platform=win32 --overwrite
+electron-packager ./ --icon=./icons/icon.png --platform=darwin --overwrite
+
+zip -r UnFootballv${1}-win32-x64.zip UnFootball-win32-x64/*
+zip -r UnFootballv${1}-linux-x64.zip UnFootball-linux-x64/*
+zip -r UnFootballv${1}-darwin-x64.zip UnFootball-darwin-x64/*
