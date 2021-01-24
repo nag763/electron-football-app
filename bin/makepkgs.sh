1f975e61fc5a13e847f40a5d06a3ea3ac9ba7b8b
nag763 <loic.labeye@tutanota.de>
2021-01-24 16:20:00 +0100

    Add icons for pkgers

--- bin/makepkgs.sh
+++ bin/makepkgs.sh
@@ -1,8 +1,8 @@
 #!usr/bin/sh
 
-electron-packager ./ --icon=./icons/icon.png --platform=linux --overwrite
-electron-packager ./ --icon=./icons/icon.png --platform=win32 --overwrite
-electron-packager ./ --icon=./icons/icon.png --platform=darwin --overwrite
+electron-packager ./ --icon=../icons/icon.png --platform=linux --overwrite
+electron-packager ./ --icon=../icons/icon.ico --platform=win32 --overwrite
+electron-packager ./ --icon=../icons/icon.icns --platform=darwin --overwrite
 
 zip -r UnFootballv${1}-win32-x64.zip UnFootball-win32-x64/*
 zip -r UnFootballv${1}-linux-x64.zip UnFootball-linux-x64/*
